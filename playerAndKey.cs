d7ea131b7de72eeec344cb4347b7b62a133b5178
thelockymichael <thelockymichael@gmail.com>
2018-12-11 16:31:37 +0200

    Update and rename Player.cs to playerAndKey.cs

--- playerAndKey.cs
+++ playerAndKey.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Player : MonoBehaviour
+public class playerAndKey : MonoBehaviour
 {
     public float speed;
     public Text countText;
